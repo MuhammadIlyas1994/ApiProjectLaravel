8ad20e8df72349742b0051f7b2f0be1deef175b5
“[firstname <yousaf@ctproducts.net>
2019-08-22 16:13:08 +0530

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Product;
+use Illuminate\Http\Request;
+
+use App\Http\Requests\ProductRequest;
+use App\Http\Resources\Product\ProductResource;
+use App\Http\Resources\Product\ProductCollection;
+
+class ProductController extends Controller
+{
+
+     public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return ProductCollection::collection(Product::paginate(8));
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProductRequest $request)
+    {
+        $product=new Product([
+            'name'=>$request->name,
+            'detail'=>$request->description,
+            'price'=>$request->price,
+            'discount'=>$request->discount,
+            'stock'=>$request->stock,
+        ]);
+        $product->save();
+        return response([
+            'data' => new ProductResource($product)
+        ],201);
+       
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        
+      return new ProductResource($product);
+    }
+
+   
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ProductRequest $request, Product $product)
+    {
+        $product->update($request->all());
+        return response([
+            'data' => new ProductResource($product)
+        ],201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
